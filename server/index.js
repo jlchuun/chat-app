6731ff4fd0e6266559eb07917140ccb27ecf1875
jlchuun <jlchuun@gmail.com>
2023-03-10 15:17:55 -0800

    Initial server/web socket setup

--- server/index.js
+++ server/index.js
@@ -1,1 +1,20 @@
 const express = require("express");
+const { WebSocketServer } = require("ws");
+
+
+const app = express();
+const server = require("http").createServer(app);
+
+const wss = new WebSocketServer({ server });
+wss.on("connection", (ws) => {
+    ws.on("message", (message) => {
+        console.log("received: %s", message);
+        ws.send(`You sent: ${message}`);
+    });
+
+    ws.send("Sent from WebSocket server");
+})
+
+server.listen(4000, () => {
+    console.log("Server listening on port 4000");
+});
