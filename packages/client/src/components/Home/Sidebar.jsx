04fa912d0ef18bab1fa14b722f5b046f34845ef3
jlchuun <jlchuun@gmail.com>
2023-03-20 13:39:06 -0700

    feat: temp add friend button

--- packages/client/src/components/Home/Sidebar.jsx
+++ packages/client/src/components/Home/Sidebar.jsx
@@ -4,6 +4,11 @@ import User from "./User";
 const Sidebar = () => {
     return (
         <aside className={styles.sidebar}>
+            <form className={styles.addFriend}>
+                <label for="username">Add Friend</label>
+                <input type="text" placeholder="Enter username" required></input>
+                <button type="submit">Send Friend Request</button>
+            </form>
             <ul>
                 <User />
                 <User />
